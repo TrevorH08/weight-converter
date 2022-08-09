99c1def773efa5f4aab0afc72ab45f46fc74c1ae
TrevorH08 <trevorh8700@gmail.com>
2022-08-09 09:43:14 -0700

    add pounds to grams calculator to page
    Co-authored-by: Tiberius Lockett <tlockett680@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 function weightConverter(valNum) {
   document.getElementById("outputGrams").innerHTML=valNum/0.035274;
 }
+function weightConverterPounds(valNum) {
+  document.getElementById("lbs2gm").innerHTML=valNum/0.022046;
+}
